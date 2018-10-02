501e281cd451b0e378061bed6db7279840b3df3b
alonso <alonso.m144@gmail.com>
2018-10-02 00:40:23 -0500

    change port

--- src/config.js
+++ src/config.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   // Node.js app
-  port: process.env.PORT || 3301,
+  port: process.env.PORT || 3001,
 
   // API Gateway
   api: {
     // API URL to be used in the client-side code
     clientUrl: process.env.API_CLIENT_URL || '',
     // API URL to be used in the server-side code
-    serverUrl: process.env.API_SERVER_URL || `http://localhost:${process.env.PORT || 3301}`,
+    serverUrl: process.env.API_SERVER_URL || `http://localhost:${process.env.PORT || 3000}`,
   },
 
   // Database
